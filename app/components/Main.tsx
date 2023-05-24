1607df6da0522d66cad0d848501b3da30cd5a692
samiul-sheikh <sheikh.samiul1971@gmail.com>
2023-05-25 03:12:19 +0600

    make navbar functional

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,25 +1,38 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 export default function Main() {
   return (
     <div className="w-full h-screen text-center">
       <div className="max-w-[1248px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div className="">
-          <p className="uppercase tracking-widest text-gray-600">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
             Lets build something together
           </p>
           <h1 className="py-4 text-gray-700">
-            Welcome to <span className=" text-[#5651eb]">Payload IT</span>
+            Welcome to <span className=" text-[#5651e5]">Payload IT</span>
           </h1>
           <h1 className="py-2 text-gray-700">Unique Software Solution</h1>
-          <p className="py-4 text-gray-600">
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
             laudantium dolor ut quisquam sunt ipsam aspernatur omnis maxime
-            minima, laborum eligendi, ex natus. Cupiditate laborum nobis
-            voluptatibus, culpa nulla doloribus mollitia sit maxime dolore,
-            animi sapiente illum esse dolorem earum eaque aliquam aliquid
-            quisquam beatae quis placeat. Repellat, est ab?
+            minima, laborum eligendi, ex natus. Cupiditate laborum nobis.
           </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4 space-x-2">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaTwitter />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,16 @@
+"use client";
 import React, { useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import Image from "next/image";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
-  // const [nav, setNave] = useState(false);
+  const [nav, setNave] = useState(false);
 
-  // const handleNav = () => {
-  //   setNave(true);
-  // };
+  const handleNav = () => {
+    setNave(!nav);
+  };
 
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
@@ -44,13 +45,23 @@ export default function Navbar() {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
-      <div className="md:hidden fixed left-0 top-0 w-full h-screen bg-black/70">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : " "
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
           <div>
             <div className="flex w-full items-center justify-between">
               <Image
@@ -59,7 +70,10 @@ export default function Navbar() {
                 width="87"
                 height="35"
               />
-              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
                 <AiOutlineClose />
               </div>
             </div>
